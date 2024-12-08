92329dc4f212da43076e8cb1a1dbadc3b6065ccb
zonnnen <95163989+zonnnen@users.noreply.github.com>
2024-12-08 22:31:33 +0300

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        private string[,] board = new string[3, 3]; 
+
+        public string[,] Board
+        {
+            get => board;
+            private set => board = value;
+        }
+
+        private string currentPlayer = "X";
+
+        public string CurrentPlayer
+        {
+            get => currentPlayer;
+            private set => currentPlayer = value;
+        }
+
+        public Game()
+        {
+            ResetGame();
+        }
+
+        public void ResetGame()
+        {
+            Board = new string[3, 3];
+            CurrentPlayer = "X";
+        }
+
+        public bool MakeMove(int row, int col)
+        {
+            if (Board[row, col] == null)
+            {
+                Board[row, col] = CurrentPlayer;
+                SwitchPlayer();
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckWinner(string currPlayer)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (Board[i, 0] == currPlayer && Board[i, 1] == currPlayer && Board[i, 2] == currPlayer) //горизонтали
+                    return true;
+
+                if (Board[0, i] == currPlayer && Board[1, i] == currPlayer && Board[2, i] == currPlayer) //вертикали
+                    return true;
+            }
+
+            //диагонали
+            if (Board[0, 0] == currPlayer && Board[1, 1] == currPlayer && Board[2, 2] == currPlayer)
+                return true;
+
+            if (Board[0, 2] == currPlayer && Board[1, 1] == currPlayer && Board[2, 0] == currPlayer)
+                return true;
+
+            return false;
+        }
+
+        public bool CheckDraw()
+        {
+            foreach (var cell in Board)
+            {
+                if (cell == null) return false;
+            }
+            return true;
+        }
+
+        private void SwitchPlayer()
+        {
+            CurrentPlayer = CurrentPlayer == "X" ? "O" : "X";
+        }
+    }
+}
---
+++ GameView.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public class GameView : Form
+    {
+        private Game game;
+        private Button[,] buttons = new Button[3,3];
+
+        public GameView()
+        {
+            game = new Game();
+            InitializeInterface();
+        }
+
+        private void InitializeInterface()
+        {
+            this.Text = "TicTacToe";
+            this.AutoSize = true; // Автоматическая подстройка размера
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink; // Разрешаем изменять размер только в большую сторону
+
+            buttons = new Button[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j] = new Button
+                    {
+                        Width = 100,
+                        Height = 100,
+                        Location = new System.Drawing.Point(100 * i, 100 * j),
+                        Font = new System.Drawing.Font("Arial", 24),
+                        Text = string.Empty
+                    };
+                    buttons[i, j].Tag = (i, j); // Сохраняем позицию кнопки
+                    buttons[i, j].Click += OnButtonClick!;
+                    Controls.Add(buttons[i, j]);
+                }
+            }
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null && string.IsNullOrEmpty(button.Text))
+            {
+                var (row, col) = ((int, int))button.Tag!;
+                string currentPlayer = game.CurrentPlayer;
+                if (game.MakeMove(row, col))
+                {
+                    button.Text = currentPlayer;
+                    if (game.CheckWinner(currentPlayer))
+                    {
+                        MessageBox.Show($"Player {currentPlayer} wins!");
+                        game.ResetGame();
+                        ResetInterface();
+                    }
+                    else if (game.CheckDraw())
+                    {
+                        MessageBox.Show("It's a draw!");
+                        game.ResetGame();
+                        ResetInterface();
+                    }
+                }
+            }
+        }
+
+        private void ResetInterface()
+        {
+            foreach (var button in buttons)
+            {
+                button.Text = string.Empty;
+            }
+        }
+    }
+}
